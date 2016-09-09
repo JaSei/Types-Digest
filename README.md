f56ae6cc06e6560062911246733299a20cf23330
Jan Seidl <seidl@avast.com>
2016-09-09 13:13:12 +0200

    inital commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Types::Digest - It's new $module
+
+# SYNOPSIS
+
+    use Types::Digest;
+
+# DESCRIPTION
+
+Types::Digest is ...
+
+# LICENSE
+
+Copyright (C) Jan Seidl.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Jan Seidl <seidl@avast.com>
---
+++ lib/Types/Digest.pm
@@ -0,0 +1,39 @@
+package Types::Digest;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Types::Digest - It's new $module
+
+=head1 SYNOPSIS
+
+    use Types::Digest;
+
+=head1 DESCRIPTION
+
+Types::Digest is ...
+
+=head1 LICENSE
+
+Copyright (C) Jan Seidl.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Jan Seidl E<lt>seidl@avast.comE<gt>
+
+=cut
+
